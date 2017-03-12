06da549f2149f076d51123b9741753ea18d7910b
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2017-03-12 18:38:11 -0400

    Working with getting total difference ready for all arrays. Only have it working by looking at one array at a time at this point.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,10 +7,30 @@ module.exports = function(app){
 	});
 
 	app.post("/api/friends", function(req, res){
+		res.json(friendData);
+		var scores = [];
+
 		var newFriend = req.body;
-		friends.push(newFriend);
-		res.json(newFriend);
-		console.log(newFriend);
+		var newData = JSON.parse(newFriend);
+		var newScores = newData.scores;
+
+		for (var i = 0; i < friendData.length; i++) {
+			scores.push(friendData[i].scores);
+		}
+		
+		var results = [];
+		var totals = [];
+		
+		for(var i = 0; i < newScores.length; i++) {
+  			results.push(Math.abs(newScores[i] - scores[0][i]));
+		}
+			console.log(results);
+
+		var totalDifference = results.reduce(function(acc, val) {
+		  return acc + val;
+		}, 0);
+
+		console.log(totalDifference);
 
 	});
 }
