bca82df83ca6f54619e8c8d3f6b8b0464c385b48
Sarah Doran <sarahdoran@wifi_stu-10-38-64-60.uncc.edu>
2017-03-14 18:47:18 -0400

    Trying to push to Heroku.

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
-// const PORT = process.env.PORT || 3000;
+// var PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
